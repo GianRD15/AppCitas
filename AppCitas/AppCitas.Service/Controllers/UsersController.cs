6865b7bbfe612fd6d4b63bd5556b6b36ee154a50
Gian Ramirez <gramirez@hermescorp.io>
2022-09-07 07:57:44 -0500

    [ADD] get controllers

--- AppCitas/AppCitas.Service/Controllers/UsersController.cs
+++ AppCitas/AppCitas.Service/Controllers/UsersController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AppCitas.Service.Controllers
+namespace AppCitas.Service.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
+        _context = context;
+    }
+    // GET api/users
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+
+        return users;
+    }
+
+    // GET api/users/{id}
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUserById(int id)
+    {
+        var user = _context.Users.Find(id);
+
+        return user;
     }
 }
