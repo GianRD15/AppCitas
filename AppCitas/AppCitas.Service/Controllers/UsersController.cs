78dec7feb5ce8e4178b70d149737f36225448bfd
Gian Ramirez <gramirez@hermescorp.io>
2022-09-07 07:35:16 -0500

    [ADD] database

---
+++ AppCitas/AppCitas.Service/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCitas.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
