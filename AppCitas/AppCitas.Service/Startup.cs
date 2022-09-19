8d4b61dab418572b69e7600c7df4845aaf32cef9
Gian Ramirez <gramirez@hermescorp.io>
2022-09-19 07:52:10 -0500

    [ADD] jwt creation

--- AppCitas/AppCitas.Service/Startup.cs
+++ AppCitas/AppCitas.Service/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using AppCitas.Service.Data;
+using AppCitas.Service.Interfaces;
+using AppCitas.Service.Services;
 
 namespace AppCitas;
 
@@ -17,6 +19,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddScoped<ITokenService, TokenService>();
         services.AddDbContext<DataContext>(options =>
         {
             options.UseSqlite(
