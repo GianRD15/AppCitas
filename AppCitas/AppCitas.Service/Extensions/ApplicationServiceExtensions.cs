eae1142a920f9d5ededc0e5bc2d8fc028f506af6
Gian Ramirez <gramirez@hermescorp.io>
2022-09-21 07:25:53 -0500

    [FIX] code organization

---
+++ AppCitas/AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Interfaces;
+using AppCitas.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        return services;
+    }
+}
--- AppCitas/AppCitas.Service/Startup.cs
+++ AppCitas/AppCitas.Service/Startup.cs
@@ -6,6 +6,7 @@ using AppCitas.Service.Services;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using AppCitas.Service.Extensions;
 
 namespace AppCitas;
 
@@ -22,39 +23,25 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddDbContext<DataContext>(options =>
-        {
-            options.UseSqlite(
-                _config.GetConnectionString("DefaultConnection")
-                );
-        });
+        services.AddAplicationServices(_config);
         services.AddControllers();
         services.AddCors();
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>{
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                ValidateIssuer = false,
-                ValidateAudience = false
-            };
-        });
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-        });
+        services.AddIdentityServices(_config);
+        //services.AddSwaggerGen(c =>
+        //{
+        //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+        //});
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-        }
+        //if (env.IsDevelopment())
+        //{
+        //    app.UseDeveloperExceptionPage();
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+        //}
 
         app.UseHttpsRedirection();
 
