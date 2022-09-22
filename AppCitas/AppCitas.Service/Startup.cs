f15bbff3aed9c19a5c597d7a498dd1fe2bab5be9
Gian Ramirez <gramirez@hermescorp.io>
2022-09-22 07:54:43 -0500

    [ADD] Exception handler middleware

--- AppCitas/AppCitas.Service/Startup.cs
+++ AppCitas/AppCitas.Service/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using AppCitas.Service.Extensions;
+using AppCitas.Service.Middleware;
 
 namespace AppCitas;
 
@@ -36,12 +37,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        //if (env.IsDevelopment())
-        //{
-        //    app.UseDeveloperExceptionPage();
-        //    app.UseSwagger();
-        //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-        //}
+        app.UseMiddleware<ExceptionMiddleware>();
 
         app.UseHttpsRedirection();
 
