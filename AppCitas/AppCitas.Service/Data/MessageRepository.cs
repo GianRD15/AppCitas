83927e1039ed9cf2c2c42decc24bc94fa3da59ba
Gian Ramirez <gramirez@hermescorp.io>
2022-11-11 07:28:53 -0600

    [ADD] message thread

--- AppCitas/AppCitas.Service/Data/MessageRepository.cs
+++ AppCitas/AppCitas.Service/Data/MessageRepository.cs
@@ -4,6 +4,7 @@ using AppCitas.Service.Helpers;
 using AppCitas.Service.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppCitas.Service.Data;
 
@@ -48,9 +49,31 @@ public class MessageRepository : IMessageRepository
             return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
     {
-        throw new NotImplementedException();
+        var messages = await _context.Message
+            .Include(u => u.Sender). ThenInclude(p => p.Photos)
+            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            .Where(m => m.Recipient.UserName.Equals(currentUsername)
+                    && m.SenderUsername.Equals(recipientUsername)
+                    || m.Recipient.UserName.Equals(recipientUsername)
+                    && m.SenderUsername.Equals(currentUsername))
+            .ToListAsync();
+        
+        var unreadMessgaes = messages
+            .Where(m => m.DateRead == null
+                    && m.Recipient.UserName.Equals(currentUsername)).ToList();
+
+        if (unreadMessgaes.Any())
+        {
+            foreach(var message in unreadMessgaes)
+            {
+                message.DateRead = DateTime.Now;
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        return _mapper.Map<IEnumerable<MessageDto>>(messages);
     }
 
     public async Task<bool> SaveAllAsync()
