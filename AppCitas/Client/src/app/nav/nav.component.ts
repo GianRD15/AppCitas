333703c7839120b6c50e66c087a19489c6136c0d
Gian Ramirez <gramirez@hermescorp.io>
2023-02-16 07:51:58 -0600

    [ADD] toast service for error notifications

--- AppCitas/Client/src/app/nav/nav.component.ts
+++ AppCitas/Client/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { User } from '../_models/user';
-import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,21 +10,22 @@ import { Observable, of } from 'rxjs';
 })
 export class NavComponent implements OnInit{
   model: any = {}
-  currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit() : void{
-    this.currentUser$ = this.accountService.currentUser$;
+    
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      error: error=> console.error(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error=> this.toastr.error(error.error)
     });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
