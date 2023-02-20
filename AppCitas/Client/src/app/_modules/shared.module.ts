2237770f5b957beeac022e35ac9703fb6e4476a6
Gian Ramirez <gramirez@hermescorp.io>
2023-02-20 07:18:05 -0600

    [FIX] module structure refractor

---
+++ AppCitas/Client/src/app/_modules/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, 
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot(
+      {
+        timeOut: 10000,
+        positionClass: 'toast-top-center',
+        preventDuplicates: true,
+      }
+    )
+  ], 
+  exports:[
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
