67a72edd4e9b2939c89d5eb0ced8a363550715e6
Gian Ramirez <gramirez@hermescorp.io>
2023-03-06 07:42:24 -0600

    [ADD] update functionality

--- AppCitas/Client/src/app/members/member-edit/member-edit.component.ts
+++ AppCitas/Client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -11,11 +13,11 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-
+  @ViewChild('editForm') editForm: NgForm | undefined;
   member: Member | undefined;
   user: User | null = null;
   
-  constructor(private accountService:AccountService, private memberService: MembersService) { 
+  constructor(private accountService:AccountService, private toastr:ToastrService, private memberService: MembersService) { 
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user=> this.user =user
     })
@@ -30,5 +32,10 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(this.user.username).subscribe({
       next: member=> this.member = member
     })
+  }
+  
+  updateMember(){
+    this.toastr.success('Profile updated!');
+    this.editForm?.reset(this.member);
   }
 }
