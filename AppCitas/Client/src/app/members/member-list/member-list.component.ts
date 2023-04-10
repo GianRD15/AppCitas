6da44c3a7a03c59e8750d6709cf35e8a6310aac1
Gian Ramirez <gramirez@hermescorp.io>
2023-04-10 07:35:10 -0600

    [FIX] member filters

--- AppCitas/Client/src/app/members/member-list/member-list.component.ts
+++ AppCitas/Client/src/app/members/member-list/member-list.component.ts
@@ -17,18 +17,10 @@ export class MemberListComponent implements OnInit{
   members: Member[] | undefined;
   pagination:Pagination | undefined;
   userParams: UserParams | undefined;
-  user: User | undefined;
   genderList =[{value:'male', display:'Males'},{value:'female', display:'Females'}];
 
   constructor(private memeberService:MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user=>{
-        if(user){
-          this.userParams = new UserParams(user);
-          this.user=user;
-        }
-      }
-    });
+    this.userParams = this.memeberService.getUserParams();
    }
 
   ngOnInit(){
@@ -37,6 +29,7 @@ export class MemberListComponent implements OnInit{
 
   loadMembers(){
     if(!this.userParams) return;
+    this.memeberService.setUserParams(this.userParams);
     this.memeberService.getMembers(this.userParams).subscribe({
       next: response =>{
         if(response.result && response.pagination){
@@ -48,15 +41,14 @@ export class MemberListComponent implements OnInit{
   }
 
   resetFilters(){
-    if(this.user){
-      this.userParams = new UserParams(this.user);
-      this.loadMembers();
-    }
+    this.userParams = this.memeberService.resetUserParams();
+    this.loadMembers();
   }
 
   pageChanged(event: any){
     if(this.userParams && this.userParams?.pageNumber != event.page){
       this.userParams.pageNumber = event.page;
+      this.memeberService.setUserParams(this.userParams);
       this.loadMembers();
     }
   }
