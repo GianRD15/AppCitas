518e6835c0cd70726b7780f00d9bbb3621fe1151
Gian Ramirez <gramirez@hermescorp.io>
2023-05-09 07:45:47 -0600

    [ADD] like functionality from message component [FEATURE] redirect to messages

--- AppCitas/Client/src/app/members/member-detail/member-detail.component.ts
+++ AppCitas/Client/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -10,14 +14,25 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  member:Member | undefined;
+  member:Member = {} as Member;
   galleryOptions: NgxGalleryOptions[]=[];
   galleryImages: NgxGalleryImage[]=[];
+  activeTab?: TabDirective;
+  @ViewChild('memberTabs',{static: true}) memberTabs?: TabsetComponent;
+  messages: Message[] = [];
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
+  constructor(private memberService: MembersService, private route: ActivatedRoute, private messageService: MessageService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: data => this.member = data['member']
+    });
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab']  && this.selectTab(params['tab']);
+      }
+    });
 
     this.galleryOptions=[
       {
@@ -29,20 +44,26 @@ export class MemberDetailComponent implements OnInit {
         preview: false
       }
     ]
-
+    this.galleryImages = this.getImages();
   }
 
-  loadMember(){
-    const username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: member => {
-        this.member = member;
-        this.galleryImages = this.getImages();
-      }
-    })
-  }
+  // loadMember(){
+  //   const username = this.route.snapshot.paramMap.get('username');
+  //   if (!username) return;
+  //   this.memberService.getMember(username).subscribe({
+  //     next: member => {
+  //       this.member = member;
+  //       // this.galleryImages = this.getImages();
+  //     }
+  //   })
+  // }
   
+  selectTab(heading: string){
+    if(this.memberTabs){
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+    }
+  }
+
   getImages(): { small: string, medium: string, big: string}[]{
     if(!this.member) return [];
     const imagesUlrs = [];
@@ -54,5 +75,24 @@ export class MemberDetailComponent implements OnInit {
       });
     }
     return imagesUlrs;
+  }
+
+  loadMessages(){
+    if(this.member){
+      this.messageService.getMessagesTrhead(this.member.username).subscribe({
+        next: messages => this.messages = messages
+      });
+    }
+  }
+  onTabActivated(data: TabDirective){
+    this.activeTab = data;
+    if(this.activeTab?.heading === 'Messages'){
+      this.loadMessages();
+    }
+  }
+  addLike(member:Member){
+    this.memberService.addlike(member.username).subscribe({
+      next: ()=> this.toastr.success("You liked " + member.username)
+    });
   }
 }
